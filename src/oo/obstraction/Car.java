882d3d95f8c07a5fe6beb065799ad935ea726b52
PaulWen <bnbn870829@gmail.com>
2017-03-07 16:18:39 +0800

    The progress of abstraction

---
+++ src/oo/obstraction/Car.java
@@ -0,0 +1,5 @@
+package oo.obstraction;
+
+public class Car {
+
+}
---
+++ src/oo/obstraction/CarTester.java
@@ -0,0 +1,10 @@
+package oo.obstraction;
+
+public class CarTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
