ec5e6e7b8061ba50abedfbb5dd62130263c70e66
sungchian <bnbn870829@gmail.com>
2017-03-14 22:53:44 +0800

    Update Pay.java

--- src/payhelper/Pay.java
+++ src/payhelper/Pay.java
@@ -1,14 +1,15 @@
 package payhelper;
 
 public class Pay {
-	String type;
-	int fare;
-	String route;
-	String paymentMethods;
-	public Pay(String type, int fare, String route, String paymentMethods){
-		this.type = type;
+	String field;						//食衣行樂擇一//
+	int fare;							//價錢//
+	String type;						//類型：食物名＾衣服＾電影//
+	String paymentMethods;				//現金＾信用卡//
+	public Pay(String field, int fare, String type, String paymentMethods){
+		this.field = field;
 		this.fare = fare;
-		this.route = route;
+		this.type = type;
 		this.paymentMethods = paymentMethods;
 	}
+
 }
