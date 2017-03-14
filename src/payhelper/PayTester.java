4917ce72dcc5fa8d537cd2a863155d4495a22680
sungchian <bnbn870829@gmail.com>
2017-03-14 22:52:46 +0800

    Update PayTester.java

--- src/payhelper/PayTester.java
+++ src/payhelper/PayTester.java
@@ -3,7 +3,8 @@ package payhelper;
 public class PayTester {
 
 	public static void main(String[] args) {
-		Pay p = new Pay("MRT", 15, "red", "cash");
+		Pay p = new Pay("transit", 15, "MRT", "cash");
+		Pay p1 = new Pay("food", 60, "breakfast", "cash");
 	}
 
 }
