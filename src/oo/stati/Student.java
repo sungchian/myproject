068475f484fbb53b36e47b27b3551db4769d338a
user <user@es302n33>
2017-04-11 16:34:41 +0800

    nice

---
+++ src/oo/stati/Student.java
@@ -0,0 +1,25 @@
+package oo.stati;
+
+public class Student {
+	int english;
+	int math;
+	int chinese;
+	static int pass = 60;
+	public Student(int english, int math, int chinese) {
+		super();
+		this.english = english;
+		this.math = math;
+		this.chinese = chinese;
+	}
+	public void print(){
+		System.out.println(getScore);
+	}
+	private String getScore(int score){
+		if (score < pass){
+			return String.valueOf(score)+"*";
+		}else{
+			return String.valueOf(score);
+		}
+	}
+	
+}
---
+++ src/oo/stati/Tester.java
@@ -0,0 +1,12 @@
+package oo.stati;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Student stu1 = new Student(85, 73, 90);
+		Student stu2 = new Student(68, 84, 89);
+		Student stu3 = new Student(75, 99, 59);
+
+	}
+
+}
