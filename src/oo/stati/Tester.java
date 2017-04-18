90a671406a3e7b060b7914eedbed5fbf13afc885
sungchian <bnbn870829@gmail.com>
2017-04-18 20:43:42 +0800

    Update Tester.java

--- src/oo/stati/Tester.java
+++ src/oo/stati/Tester.java
@@ -6,7 +6,9 @@ public class Tester {
 		Student stu1 = new Student(85, 73, 90);
 		Student stu2 = new Student(68, 84, 89);
 		Student stu3 = new Student(75, 99, 59);
-
+		stu1.pass = 100;
+		stu1.print();
+		stu2.print();
 	}
 
 }
