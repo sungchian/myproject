295aa0faad7064a85bfdba48c145581ed69372a3
user <user@es302n33>
2017-06-06 15:56:47 +0800

    hw

---
+++ src/oo/abs2/Tester.java
@@ -0,0 +1,16 @@
+package oo.abs2;
+
+public class Tester {
+
+	public static void main(String[] args) {
+		Report report = new Report() {
+			
+			@Override
+			public void print() {
+				// TODO Auto-generated method stub
+				
+			}
+		};
+	}
+
+}
---
+++ src/oo/abs3/GradeReport.java
@@ -0,0 +1,17 @@
+package oo.abs3;
+
+	public class GradeReport implements Report{
+
+		@Override
+		public void test() {
+			// TODO Auto-generated method stub
+			
+		}
+
+		@Override
+		public void print() {
+			// TODO Auto-generated method stub
+			
+		}
+
+}
